7eb826908d67b49e4cc866be966c00a36344892c
Giang <giang.lt.62cntt@ntu.edu.vn>
2023-11-30 09:31:58 +0700

    them bt

---
+++ cau-lenh-dk/ktra-am-duong.js
@@ -0,0 +1,11 @@
+let a = 10;
+if(a<0){
+    console.log("a là số âm")
+}else{
+    console.log("a là số dương")
+}
+if(a%2==0){
+    console.log("a là số chắn")
+}else{
+    console.log("a là số lẻ")
+}
